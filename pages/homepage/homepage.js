38a5a821b037b1d989242bf2a961f32812f27e8e
Adam Schellenberg <adamschellenberg93@gmail.com>
2024-04-12 20:02:19 -0700

    refactoring and fleshing out
    refactored tools and language sections to be broken into section folders. added projects section

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,53 @@
+const toolsSection = document.getElementById("tools-root");
+const languagesSection = document.getElementById("languages-root");
+const projectsSection = document.getElementById("projects-root");
+
+let projects = [
+    "nexopedia",
+    "fightingfoodons"
+];
+
+toolsSection.innerHTML = "";
+languagesSection.innerHTML = "";
+projectsSection.innerHTML = "";
+
+
+const initializeHomepage = () => {
+    loadToolsSection();
+    loadLanguagesSection();
+    loadProjectsSection();
+}
+
+const loadToolsSection = () => {
+    const source = "/pages/homepage/sections/tools/tools.html";
+    const destination = "#tools-root";
+    loadHomepageContent(source, destination);
+}
+
+const loadLanguagesSection = () => {
+    const source = "/pages/homepage/sections/languages/languages.html";
+    const destination = "#languages-root";
+    loadHomepageContent(source, destination);
+}
+
+const loadProjectsSection = () => {
+    const sourceBase = "/pages/homepage/sections/";
+    const destination = "#projects-root";
+
+    projects.forEach((project) => {
+        let source = sourceBase +  `${project}/${project}.html`;
+        loadHomepageContent(source, destination);
+    }) 
+}
+
+const loadHomepageContent = (source, destination) => {
+    $.ajax({
+        url: source,
+        dataType: "html",
+        success: function (data) {
+            $(destination).append(data);
+        },
+    });
+}
+
+initializeHomepage();
