6abee86aa276404eff9a633064ff253aa421041f
Adam Schellenberg <adamschellenberg93@gmail.com>
2024-04-12 20:50:26 -0700

    removed commit tracker (for now)

--- pages/homepage/sections/fightingfoodons/fightingfoodons.js
+++ pages/homepage/sections/fightingfoodons/fightingfoodons.js
@@ -1,5 +1,5 @@
-fetch('https://github.com/adamschellenberg/FightingFoodonsSite/commits?per_page=1')
-  .then(res => res.json())
-  .then(res => {
-    document.getElementById('message').innerHTML = res[0].commit.message
-  })
+// fetch('https://github.com/adamschellenberg/FightingFoodonsSite/commits?per_page=1')
+//   .then(res => res.json())
+//   .then(res => {
+//     document.getElementById('message').innerHTML = res[0].commit.message
+//   })
